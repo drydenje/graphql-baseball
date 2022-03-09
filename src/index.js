8d89e7bd024cdfe4f229de4af2322d9b65f1968f
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-03-09 15:50:52 -0500

    added port listening for heroku deploy

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ const server = new ApolloServer({
   resolvers: [players.resolvers, parks.resolvers],
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
