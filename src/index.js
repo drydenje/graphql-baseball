c128ce047590041e283cc320439bd728d8f6ade8
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-03-09 14:21:02 -0500

    missed a file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const { ApolloServer, gql } = require("apollo-server");
 const players = require("./Players");
+const parks = require("./Parks");
 
 const typeDef = gql`
   type Query
 `;
 
 const server = new ApolloServer({
-  typeDefs: [typeDef, players.typeDef],
-  resolvers: [players.resolvers],
+  typeDefs: [typeDef, players.typeDef, parks.typeDef],
+  resolvers: [players.resolvers, parks.resolvers],
 });
 
 server.listen().then(({ url }) => {
