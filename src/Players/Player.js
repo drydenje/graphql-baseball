118c519d37188c06c9f37b56e1bb47b019578854
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-03-21 23:53:03 -0400

    mongodb table name didnt match graphql schema i think, search all players and player by id now working

--- src/Players/Player.js
+++ src/Players/Player.js
@@ -2,29 +2,29 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
   playerID: String,
-  birthYear: Number,
-  birthMonth: Number,
-  birthDay: Number,
-  birthCountry: String,
-  birthState: String,
-  birthCity: String,
-  nameFirst: String,
-  nameLast: String,
+  // birthYear: Number,
+  // birthMonth: Number,
+  // birthDay: Number,
+  // birthCountry: String,
+  // birthState: String,
+  // birthCity: String,
+  // nameFirst: String,
+  // nameLast: String,
   nameGiven: String,
-  weight: Number,
-  height: Number,
-  bats: String,
-  throws: String,
-  debut: String,
-  finalGame: String,
-  retroID: String,
-  bbrefID: String,
-  deathYear: Number,
-  deathMonth: Number,
-  deathDay: Number,
-  deathCountry: String,
-  deathState: String,
-  deathCity: String,
+  // weight: Number,
+  // height: Number,
+  // bats: String,
+  // throws: String,
+  // debut: String,
+  // finalGame: String,
+  // retroID: String,
+  // bbrefID: String,
+  // deathYear: Number,
+  // deathMonth: Number,
+  // deathDay: Number,
+  // deathCountry: String,
+  // deathState: String,
+  // deathCity: String,
 });
 
 const model = mongoose.model("Player", schema);
@@ -33,8 +33,16 @@ class Player {
   static async all() {
     try {
       const allPlayers = await model.find();
-      console.log("Allplayers:", allPlayers);
       return allPlayers;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  static async searchByID(id) {
+    try {
+      const foundPlayer = await model.findOne({ playerID: id });
+      return foundPlayer;
     } catch (error) {
       console.error("Error:", error);
     }
