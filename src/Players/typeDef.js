14f93836e50faead403664ce187d9add23225a96
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-03-22 10:38:29 -0400

    basic player resolver finished, might need to come back to this later

--- src/Players/typeDef.js
+++ src/Players/typeDef.js
@@ -3,29 +3,29 @@ const { gql } = require("apollo-server");
 const typeDef = gql`
   type Player {
     playerID: String
-    # birthYear: Int
-    # birthMonth: Int
-    # birthDay: Int
-    # birthCountry: String
-    # birthState: String
-    # birthCity: String
-    # nameFirst: String
-    # nameLast: String
+    birthYear: Int
+    birthMonth: Int
+    birthDay: Int
+    birthCountry: String
+    birthState: String
+    birthCity: String
+    nameFirst: String
+    nameLast: String
     nameGiven: String
-    # weight: Int
-    # height: Int
-    # bats: String
-    # throws: String
-    # debut: String
-    # finalGame: String
-    # retroID: String
-    # bbrefID: String
-    # deathYear: Int
-    # deathMonth: Int
-    # deathDay: Int
-    # deathCountry: String
-    # deathState: String
-    # deathCity: String
+    weight: Int
+    height: Int
+    bats: String
+    throws: String
+    debut: String
+    finalGame: String
+    retroID: String
+    bbrefID: String
+    deathYear: Int
+    deathMonth: Int
+    deathDay: Int
+    deathCountry: String
+    deathState: String
+    deathCity: String
   }
 
   extend type Query {
