43bcfbe50f8f0b8f31be56979423267d7eea39f7
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-03-22 13:28:16 -0400

    resolvers for appearences mostly finished, will need to come back later to incorporate queries with other resolvers. Debugging is going much quicker

---
+++ src/Appearances/index.js
@@ -0,0 +1,9 @@
+const { Appearance } = require("./Appearance");
+const { resolvers } = require("./resolvers");
+const { typeDef } = require("./typeDef");
+
+module.exports = {
+  Appearance,
+  resolvers,
+  typeDef,
+};
