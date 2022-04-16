59d63506e03addbb6ada6208dc7f2ab4146a5240
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-04-16 15:48:28 -0400

    fixed Batting resolver object, cleaned up some old mocked data

--- src/Batting/Batting.js
+++ src/Batting/Batting.js
@@ -3,26 +3,26 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
   playerID: String,
   yearID: Number,
-  // stint: Number,
-  // teamID: String,
-  // lgID: String,
-  // iG: Number,
-  // iAB: Number,
-  // iR: Number,
-  // iH: Number,
-  // i2B: Number,
-  // i3B: Number,
-  // iHR: Number,
-  // iRBI: Number,
-  // iSB: Number,
-  // iCS: Number,
-  // iBB: Number,
-  // iSO: Number,
-  // iIBB: Number,
-  // iHBP: Number,
-  // iSH: Number,
-  // iSF: Number,
-  // iGIDP: Number,
+  stint: Number,
+  teamID: String,
+  lgID: String,
+  iG: Number,
+  iAB: Number,
+  iR: Number,
+  iH: Number,
+  i2B: Number,
+  i3B: Number,
+  iHR: Number,
+  iRBI: Number,
+  iSB: Number,
+  iCS: Number,
+  iBB: Number,
+  iSO: Number,
+  iIBB: Number,
+  iHBP: Number,
+  iSH: Number,
+  iSF: Number,
+  iGIDP: Number,
 });
 
 const model = mongoose.model("Batting", schema);
@@ -39,37 +39,8 @@ class Batting {
 
   static async searchByPlayerID(id) {
     try {
-      console.log(id);
       const foundBattingLines = await model.find({ playerID: id });
-      console.log(foundBattingLines);
-      // return foundBattingLines;
-      return [
-        {
-          _id: { $oid: "6259ddd9ff6c628f6d62df7c" },
-          playerID: "rojasjo01",
-          yearID: 2019,
-          stint: 1,
-          teamID: "ARI",
-          lgID: "NL",
-          iG: 41,
-          iAB: 138,
-          iR: 17,
-          iH: 30,
-          i2B: 7,
-          i3B: 0,
-          iHR: 2,
-          iRBI: 16,
-          iSB: 4,
-          iCS: 2,
-          iBB: 18,
-          iSO: 41,
-          iIBB: 0,
-          iHBP: 1,
-          iSH: 0,
-          iSF: 0,
-          iGIDP: 3,
-        },
-      ];
+      return foundBattingLines;
     } catch (error) {
       console.error("Error:", error);
     }
